9d998c4028c97c952fee7edb5666ca9bd9735609
Eliza Munroe <era3munroe@gmail.com>
2019-08-12 21:17:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ class Song
   def self.new_from_filename(filename)
     split = filename.split(' - ')
     artist_name = split[0]
-    name = split[1](' . ')
+    name = split[1].split('.')[0]
     binding.pry
 
   end
