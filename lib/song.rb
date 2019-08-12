876e9ed8759605e7a7d00f1c4ae8079e53aed1fa
Eliza Munroe <era3munroe@gmail.com>
2019-08-12 21:06:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,58 @@
+'require pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
 
   def self.all
     @@all
+  end
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    new_song = self.new
+    new_song.name = name
+    new_song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    if song = self.find_by_name(name)
+      song
+    else
+      self.create_by_name(name)
+    end
+  end
+  
+  def self.new_from_filename(name)
+    filename = name.split(' - ')
+    binding.pry
+
+  end
+  
+  def self.alphabetical
+    self.all.sort { |a, b| a.name <=> b.name}
   end
 
   def save
     self.class.all << self
   end
 
+  def self.destroy_all
+    self.all.clear
+  end
 end
